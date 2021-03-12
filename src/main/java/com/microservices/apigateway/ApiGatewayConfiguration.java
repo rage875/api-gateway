1efabc384292a5532a4a0c8fada7e73746635497
rage875 <rage.875@gmail.com>
2021-03-12 09:27:16 -0600

    Logging filter added

--- src/main/java/com/microservices/apigateway/ApiGatewayConfiguration.java
+++ src/main/java/com/microservices/apigateway/ApiGatewayConfiguration.java
@@ -1,11 +1,6 @@
 package com.microservices.apigateway;
 
-import java.util.function.Function;
-
-import org.springframework.cloud.gateway.route.Route;
 import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.Buildable;
-import org.springframework.cloud.gateway.route.builder.PredicateSpec;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
